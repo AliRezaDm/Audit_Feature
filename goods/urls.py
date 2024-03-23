32834fa69c121d3d519e911e1a48739941221f67
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-03-24 01:02:40 +0330

    Add category view and templates

--- goods/urls.py
+++ goods/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Supplylist
+from .views import Supplylist, CategoryList
 
 
 
 urlpatterns = [
-    path('', Supplylist.as_view(), name='supply_list')
+    path('', Supplylist.as_view(), name='supply_list'), 
+     path('category/', CategoryList.as_view(), name='category_list')
 ]
 
--- goods/views.py
+++ goods/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import Supply
+from .models import Supply, Category
 
 class Supplylist(ListView):
 
     model = Supply
     queryset = Supply.objects.Available()
     template_name = 'goods/supply_list.html'
+
+
+class CategoryList(ListView):
+
+    model = Category
+    queryset = Category.objects.filter(status=True)
+    template_name = "goods/category_list"
