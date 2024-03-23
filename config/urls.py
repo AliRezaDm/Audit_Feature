8175c128aa14c75d02b8aabf7f691ac4cd975164
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-03-24 00:42:48 +0330

    Update ulrs.py for allauth

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     #all auth
-    path('accounts/', include('allauth.urls')),
+    path('', include('allauth.urls')),
     
     path('', include('goods.urls'))
 ]
