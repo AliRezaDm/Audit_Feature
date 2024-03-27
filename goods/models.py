1edfd332a4b5a82545761f9507fc2945f170ccf7
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-03-28 02:09:59 +0330

    Update goods model

--- goods/models.py
+++ goods/models.py
@@ -38,6 +38,8 @@ class Supply(models.Model):
     image = models.ImageField(verbose_name='تصویر محصول', upload_to='media', null=True)
     category = models.ManyToManyField(Category, verbose_name='دسته بندی', related_name='cat')
     title = models.CharField(max_length=100, verbose_name='نام محصول')
+    color = models.ManyToManyField('Color' ,related_name='goods', verbose_name='رنگ', null=True, blank=True)
+    size = models.ManyToManyField('Size', related_name='goods', verbose_name='اندازه', null=True, blank=True)
     status = models.CharField(max_length=1, verbose_name='وضعیت محصول', choices=STATUS_CHOICE)
     count = models.IntegerField(verbose_name='تعداد')
 
@@ -58,4 +60,36 @@ class Supply(models.Model):
          return html.format_html("<img width=100; height=75; style='border-radius:10px;'src='{}'>".format(self.image.url))
     image_tag.short_description = short_decsription = 'تصویر محصول'
     
+    def colors_to_str(self):
+          return " - ".join([color.name for color in self.colors.all()])
+    colors_to_str.short_description = 'رنگ ها'
 
+    def sizes_to_str(self):
+        return " - ".join([size.name for size in self.sizes.all()])
+    sizes_to_str.short_description = 'اندازه ها'
+
+
+class Color(models.Model):
+    
+    name = models.CharField('نام رنگ', max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+      
+        verbose_name = 'رنگ'
+        verbose_name_plural = 'رنگ ها'
+
+
+class Size(models.Model):
+    
+    name = models.CharField('عنوان اندازه', max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        
+        verbose_name = 'اندازه'
+        verbose_name_plural = 'اندازه ها'
